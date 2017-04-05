a32b1fece215dcf284b7f0847daa21bdb8283d0d
jim-graham <grahamfamily.jim@gmail.com>
2017-04-05 15:10:00 -0500

    Removed the storyboards and set initial view

--- SwiftTabBarControllerTutorial/AppDelegate.swift
+++ SwiftTabBarControllerTutorial/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let homeViewController = HomeViewController()
+        homeViewController.view.backgroundColor = UIColor.cyan
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
+        
         return true
     }
 
@@ -40,7 +48,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
