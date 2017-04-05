62d42a1323f1de72a9b1bc7c9f5da1600eb72a61
jim-graham <grahamfamily.jim@gmail.com>
2017-04-05 14:32:38 -0500

    Added Firebase/Core, Firebase/Auth, ReSwift, and Swinject pods

--- Podfile
+++ Podfile
@@ -7,7 +7,14 @@ target 'SwiftTabBarControllerTutorial' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
+  source 'https://github.com/CocoaPods/Specs.git'
+
   # Pods for SwiftTabBarControllerTutorial
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'ReSwift'
+  pod 'Swinject', '~> 2.0.0'
+
 
   target 'SwiftTabBarControllerTutorialTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-SwiftTabBarControllerTutorial/Pods-SwiftTabBarControllerTutorial-frameworks.sh
+++ Pods/Target Support Files/Pods-SwiftTabBarControllerTutorial/Pods-SwiftTabBarControllerTutorial-frameworks.sh
@@ -87,6 +87,19 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReSwift/ReSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Swinject/Swinject.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReSwift/ReSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Swinject/Swinject.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
