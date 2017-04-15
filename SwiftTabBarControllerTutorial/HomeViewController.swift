3873d655dfd008de3a9eb6c314f3e5bcc025bf3b
jim-graham <grahamfamily.jim@gmail.com>
2017-04-14 22:17:44 -0500

    Added auto layout constraints for a label

--- SwiftTabBarControllerTutorial/HomeViewController.swift
+++ SwiftTabBarControllerTutorial/HomeViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+
+    private var labelViewSize: CGSize = CGSize(width: 0.0, height: 0.0)
+
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "jim"
+        label.font = UIFont.boldSystemFont(ofSize: 24.0)
+        label.textAlignment = NSTextAlignment.center
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.white
+
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.cyan
-    }
+        
+        view.addSubview(statusLabel)
+        statusLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        statusLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        statusLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
